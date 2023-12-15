183a95d003875a24efa4e8b2e4e21bd3f26ff45a
Verch12 <153327209+Verch12@users.noreply.github.com>
2023-12-15 16:06:45 +0300

    SQL UWU

--- SQLpy.py
+++ SQLpy.py
@@ -1,5 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect('lessons.db')
+con = sqlite3.connect('lessons.db')
+cur = con.cursor()
+cur.execute("SELECT * FROM lessons")
 
-conn.close()
+for person in cur.fetchall():
+    print(person[0], person[1], person[2], person[3], person[4])
+
+con.close()
